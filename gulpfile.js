e7e2b20d24c06c392e4d5b6c362f67928ddbfe18
Jamy Golden <jamy@22seven.com>
2015-02-20 13:54:11 +0200

    added package.json items. fixed gulp file to compile es6 correctly. Sourcemaps work nicely with chrome breakpoints

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@
     var gulp = require('gulp');
     var sourcemaps = require('gulp-sourcemaps');
     var compass = require('gulp-compass');
-    var traceur = require('gulp-traceur');
     var concat = require('gulp-concat');
+    var babel = require('gulp-babel');
+
+    // For prod one day
+    // var uglify = require('gulp-uglify');
+    // var clean = require('gulp-clean');
 
     gulp.task('compass', function() {
-        gulp.src('./src/scss/*.scss')
+        return gulp.src('./src/scss/*.scss')
             .pipe(compass({
                     config_file: './config.rb',
                     css: 'src/assets/css',
@@ -15,14 +19,26 @@
             .pipe(gulp.dest('src/assets/css'));
     });
 
-    gulp.task('js', function() {
-        gulp.src('./src/js/**/*.js')
+    gulp.task('js', function () {
+        return gulp.src('src/js/**/*.js')
             .pipe(sourcemaps.init())
-                .pipe(traceur())
-                .pipe(concat('app.js'))
+            .pipe(babel())
+            .pipe(concat('app.js'))
             .pipe(sourcemaps.write('.'))
             .pipe(gulp.dest('src'));
     });
+
+    // For prod one day
+    // gulp.task('prod', function () {
+    //     gulp.src('prod/', {read: false})
+    //         .pipe(clean());
+
+    //     return gulp.src('src/js/*.js')
+    //         .pipe(babel())
+    //         .pipe(concat('app.js'))
+    //         .pipe(uglify())
+    //         .pipe(gulp.dest('prod'));
+    // });
 
     gulp.task('default', function() {
         gulp.watch('src/scss/**.scss', ['compass']);
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,23 @@
 (function() {
-    'use strict';
+
+    class Person {
+
+        constructor(args) {
+            this.name = args.name;
+            this.age = args.age;
+        }
+
+        render() {
+            console.log(`${this.name} is ${this.age} years old`);
+        }
+    }
+
+    var jamy = new Person({
+        name: 'Jamy',
+        age: 27
+    });
+
+    jamy.render();
 
     // Game here
 })();
