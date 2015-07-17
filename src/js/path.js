69ed7a05af3ebb175d3d36773ce768acf82b6cf6
Jamy Golden <jamygolden@gmail.com>
2015-07-17 16:01:20 +0200

    added new base files to build custom path finding

---
+++ src/js/path.js
@@ -0,0 +1,13 @@
+(function() {
+
+    var canvas = document.createElement('canvas');
+    var CANVAS_HEIGHT = 400;
+    var CANVAS_WIDTH = 400;
+
+    canvas.className = 'Canvas';
+    canvas.height = CANVAS_HEIGHT;
+    canvas.width = CANVAS_WIDTH;
+
+    document.body.appendChild(canvas);
+
+})();
